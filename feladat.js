e494c6f0cca89be55fef02bef7c5ebea86ef03c7
suzo777 <suzo777@gmail.com>
2025-09-20 10:08:42 +0200

    Update feladat.js

--- feladat.js
+++ feladat.js
@@ -13,10 +13,16 @@ res.end();
 
 function adatb(fuggveny) {
 var connection = mysql.createConnection({
+    host: "dpg-d375rbffte5s73avaeag-a",
+    user: "nodejs1_user",
+    password: "2NlfSGty4r1woCFSrCJC2nY7gyzFOhRt",
+    database : 'nodejs1'
+    /*
     host: "sql.freedb.tech",
     user: "freedb_proba1",
     password: "FEFF@4hQ7&8Tmzt",
    database : 'freedb_proba123'
+   */
  });
 connection.connect(function(err){
  if(!err) {
