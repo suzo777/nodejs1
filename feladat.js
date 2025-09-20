e17f30798173ab2d87171d55d055edf7f3f91660
suzo777 <suzo777@gmail.com>
2025-09-20 16:02:40 +0200

    Update feladat.js

--- feladat.js
+++ feladat.js
@@ -1,11 +1,40 @@
+const http = require('http'); 
+//var mysql      = require('mysql2');
 var pg = require('pg');
-//var conString = "postgres://YourUserName:YourPassword@localhost:5432/YourDatabase";
+console.log("Kapcsolódás az adatbázishoz ... "); 
+console.log("Böngészőben: IP-cím:8080 ... ");  
+
+const server = http.createServer((req, res) => { 
+adatb( (text) => {  
+res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+res.write(text);
+res.end();
+});
+}).listen(8080);
+
+function adatb(fuggveny) {
+    var conString = "postgresql://nodejs1_user:2NlfSGty4r1woCFSrCJC2nY7gyzFOhRt@dpg-d375rbffte5s73avaeag-a/nodejs1";
+    var client = new pg.Client(conString);
+    //client.connect();
+    client.connect(function(err){
+    if(!err) {
+    var text = " Database is connected ... ";
+    } else {
+    var text = " Error connecting database ... ";
+    }
+            fuggveny(text);	
+    });
+}
+
+/*
+var pg = require('pg');
 var conString = "postgresql://nodejs1_user:2NlfSGty4r1woCFSrCJC2nY7gyzFOhRt@dpg-d375rbffte5s73avaeag-a/nodejs1";
 var client = new pg.Client(conString);
 client.connect();
+*/
 
-const result = await client.query('SELECT NOW()')
-console.log(result)
+//const result = await client.query('SELECT NOW()')
+//console.log(result)
 //await client.end()
 
 
