925e261a9d9ee5768d777942242376ff4de5fd4b
suzo777 <suzo777@gmail.com>
2025-09-20 07:57:32 +0200

    Update feladat.js

--- feladat.js
+++ feladat.js
@@ -1,10 +1,29 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http'); 
+var mysql      = require('mysql2');
+console.log("Kapcsolódás az adatbázishoz ... "); 
+console.log("Böngészőben: IP-cím:8080 ... ");  
 
-http.createServer(function (req, res) {
-    fs.readFile('index.html', 'utf8', function(err, data){ 
-        res.end(data);
-    });       
-    console.log('Fájl beolvasva'); 
+const server = http.createServer((req, res) => { 
+adatb( (text) => {  
+res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+res.write(text);
+res.end();
+});
 }).listen(8080);
 
+function adatb(fuggveny) {
+var connection = mysql.createConnection({
+    host: "sql.freedb.tech",
+    user: "freedb_proba1",
+    password: "FEFF@4hQ7&8Tmzt",
+   database : 'freedb_proba123'
+ });
+connection.connect(function(err){
+ if(!err) {
+var text = " Database is connected ... ";
+ } else {
+var text = " Error connecting database ... ";
+ }
+		fuggveny(text);	
+ });
+}
